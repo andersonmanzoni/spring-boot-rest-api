cf0aa0d6b8045697666f9869742b9006b66243f1
Anderson Manzoni <anderson.manzonii@gmail.com>
2024-03-14 15:05:44 -0400

    Removed files

--- src/main/java/app/rest/repository/ItemAttributeRepository.java
+++
@@ -1,8 +0,0 @@
-package app.rest.repository;
-
-import app.rest.entity.ItemAttribute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemAttributeRepository extends JpaRepository<ItemAttribute, Integer> {
-
-}
--- src/main/java/app/rest/repository/ItemItemAttributeRepository.java
+++
@@ -1,8 +0,0 @@
-package app.rest.repository;
-
-import app.rest.entity.ItemItemAttribute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemItemAttributeRepository extends JpaRepository<ItemItemAttribute, Integer> {
-
-}
