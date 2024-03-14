6f1789c06855e9be96781f2f9431b2467e206703
Anderson Manzoni <anderson.manzonii@gmail.com>
2024-03-14 18:23:03 -0400

    After configure gradle to generate class files from wsdl file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("com.github.bjornvester.wsdl2java") version "2.0.2"
 }
 
 group = "rest-api"
@@ -20,6 +21,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	}
+
+wsdl2java {
+	wsdlDir.set(layout.projectDirectory.dir("src/main/resources/wsdl"))
 }
 
 tasks.withType<Test> {
